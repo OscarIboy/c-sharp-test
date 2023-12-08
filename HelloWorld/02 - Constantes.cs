9fe6c3d16646df863e64fd66cf13aa1457dcecba
Oscar <oscar@r-digital.tech>
2023-12-07 18:05:24 -0600

    Constantes

---
+++ HelloWorld/02 - Constantes.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hola Mundo");
+
+var radio = 0d;
+var resultado = 0d;
+
+const double Pi = 3.1416;
+
+Console.WriteLine("Bienvenido, calculemos círculos!");
+
+// Pi * radio * radio
+
+Console.WriteLine("Ingrese el valor del radio");
+radio = Convert.ToDouble(Console.ReadLine());
+
+resultado = Pi * (radio * radio);
+// resultado = Math.PI * (radio * radio);
+
+Console.WriteLine("El área de tu circulo es de: " + resultado);
